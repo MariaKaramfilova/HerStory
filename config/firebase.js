29b4e9ff5967fb0b09a7d96ba1a70e8bced80757
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-08-02 11:55:14 +0300

    Added firebase account config

---
+++ config/firebase.js
@@ -0,0 +1,20 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTCw153TnKVVCTqpGhk0Sctbyeq-oy7gQ",
+  authDomain: "herstory-forum-app-dev.firebaseapp.com",
+  databaseURL: "https://herstory-forum-app-dev-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "herstory-forum-app-dev",
+  storageBucket: "herstory-forum-app-dev.appspot.com",
+  messagingSenderId: "127971481183",
+  appId: "1:127971481183:web:af06934f43056d51e0edea"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export default app;
