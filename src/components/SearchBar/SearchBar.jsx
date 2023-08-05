54781b323128d55c6f1342a568c7efdb000144bb
maria_karamfilova <karamfilova.mgeorgieva@gmail.com>
2023-08-05 18:28:20 +0300

    Small update to fix issue when making blank search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,26 +4,35 @@ import { useNavigate } from 'react-router-dom';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [showError, setShowError] = useState(false);
   const navigate = useNavigate();
 
   const handleSearchClick = (e) => {
     e.preventDefault();
-    navigate(`/search/${searchTerm}`);
-    setSearchTerm('');
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+      setShowError(false);
+    } else {
+      setShowError(true);
+    }
+
   }
 
   return (
-    <Form style={styles.form}>
-      <FormControl
-        className="search-bar"
-        type="text"
-        value={searchTerm}
-        name="topic"
-        onChange={(e) => setSearchTerm(e.target.value)}
-        placeholder="Enter a topic"
-      />
-      <Button type="submit" variant="dark" onClick={handleSearchClick} style={styles.button}>Search</Button>
-    </Form>
+    <>
+      <Form style={styles.form}>
+        <FormControl
+          className="search-bar"
+          type="text"
+          value={searchTerm}
+          name="topic"
+          onChange={(e) => {setSearchTerm(e.target.value); setShowError(false)}}
+          placeholder={showError ? "Please enter a search term" : "Enter a topic"}
+        />
+        <Button type="submit" variant="dark" onClick={handleSearchClick} style={styles.button}>Search</Button>
+      </Form>
+    </>
   );
 };
 
@@ -45,6 +54,7 @@ const styles = {
   button: {
     margin: "15px",
   }
+  
 
 };
 
