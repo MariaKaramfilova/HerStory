249fb1484c3b1d011d87c66d40750ab87c42d738
Hristiyan Fachikov <hristiyan.fachikov>
2023-08-10 00:11:10 +0300

    Add icon and title

--- src/components/Posts/PostsDetails.jsx
+++ src/components/Posts/PostsDetails.jsx
@@ -3,11 +3,21 @@ import moment from "moment";
 import { Card, Image, Button } from "react-bootstrap";
 import { getUserByUsername } from "../../services/users.services";
 import MyAccount from "../../views/Account/Account";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 export default function PostsDetails({ goToDetails, ...post }) {
   const [authorData, setAuthorData] = useState(null);
   const [typeFile, setTypeFile] = useState("");
+
+  const [voteStatus, setVoteStatus] = useState(null);
+
+  const handleVote = (voteType) => {
+    if (voteStatus === voteType) {
+      setVoteStatus(null);
+    } else {
+      setVoteStatus(voteType);
+    }
+  };
 
   const navigate = useNavigate();
 
@@ -21,7 +31,10 @@ export default function PostsDetails({ goToDetails, ...post }) {
         if (post.file) {
           if (post.file.includes("mp4")) {
             setTypeFile("video");
-          } else if (post.file.includes('images') && !post.file.includes('mp4')){
+          } else if (
+            post.file.includes("images") &&
+            !post.file.includes("mp4")
+          ) {
             setTypeFile("image");
           }
         }
@@ -35,7 +48,7 @@ export default function PostsDetails({ goToDetails, ...post }) {
   console.log(post);
 
   if (false) {
-    return <MyAccount userName={post.userName} />
+    return <MyAccount userName={post.userName} />;
   }
 
   return (
@@ -59,22 +72,61 @@ export default function PostsDetails({ goToDetails, ...post }) {
         <Card.Subtitle className="mb-2 text-muted">
           {moment(post.createdOn).toString()}
         </Card.Subtitle>
-        <Card style={{ fontSize: "17px", marginTop: '15px', backgroundColor: 'WhiteSmoke', paddingLeft:'5px', paddingRight:'5px' }}>{post.content}</Card>
+        <Card
+          style={{
+            fontSize: "17px",
+            marginTop: "15px",
+            backgroundColor: "WhiteSmoke",
+            paddingLeft: "5px",
+            paddingRight: "5px",
+          }}
+        >
+          {post.content}
+        </Card>
         <div className={`media-element ${typeFile}`}>
-        <Card >
+          <Card>
             {typeFile === "image" && (
               <Image src={post.file} fluid style={{ width: "60%" }} />
             )}
             {typeFile === "video" && (
               <video controls className="media-element">
                 <source src={post.file} type="video/mp4" />
-                Your browser does not support the video tag.
               </video>
             )}
           </Card>
         </div>
-        <hr/>
-        <Button type="submit"  variant="dark" onClick={() => navigate(`/detailed-post-view/${post.id}`)}>Comment</Button>
+        <hr />
+        <Button
+          type="submit"
+          variant={voteStatus === "up" ? "danger" : "dark"}
+          onClick={() => handleVote("up")}
+          style={{ marginRight: "7px" }}
+        >
+          ▲
+        </Button>
+        <span
+          type="submit"
+          onClick={() => navigate(`/detailed-post-view/${post.id}`)}
+          style={{ marginRight: "7px" }}
+        >
+          156
+        </span>
+        <Button
+          type="submit"
+          variant={voteStatus === "down" ? "danger" : "dark"}
+          onClick={() => handleVote("down")}
+          style={{ marginRight: "15px" }}
+        >
+          ▼
+        </Button>
+        <Button
+          type="submit"
+          variant="dark"
+          onClick={() => navigate(`/detailed-post-view/${post.id}`)}
+          style={{ marginRight: "5px" }}
+        >
+          Comment
+        </Button>
       </Card.Body>
     </Card>
   );
