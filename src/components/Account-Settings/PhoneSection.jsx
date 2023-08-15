c1539bb30af44d7360a1b8f83c760926a4b8d7aa
Hristiyan Fachikov <hristiyan.fachikov>
2023-08-15 22:12:24 +0300

    Add a PhoneSection component

--- src/components/Account-Settings/PhoneSection.jsx
+++ src/components/Account-Settings/PhoneSection.jsx
@@ -1,21 +1,55 @@
 import { reauthenticateWithCredential } from "firebase/auth";
 import { EmailAuthProvider } from "firebase/auth";
 import { updateProfilePhone } from "../../services/users.services";
+import { Button, Card, Form } from 'react-bootstrap';
+import { getAllUsers } from "../../services/users.services";
+import { useState } from "react";
+export default function PhoneSection({ loggedInUser, user, setUser }) {
 
-export const PhoneChanging = async (user, loggedInUser, phone, username, setPhone) => {
-  const password = prompt("Please enter your password to confirm phone change:");
+  const [phone, setPhone] = useState("");
 
-  if (password) {
-    const credentials = EmailAuthProvider.credential(loggedInUser.email, password);
-    try {
-      await reauthenticateWithCredential(user, credentials);
-      if (phone) {
-        await updateProfilePhone(phone, username);
-        alert("Congratulations! You successfully changed your phone!");
-        setPhone(phone);
+  const changePhone = async () => {
+    const password = prompt(
+      "Please enter your password to confirm email change:"
+    );
+    if (password) {
+      const credentials = EmailAuthProvider.credential(loggedInUser.email, password);
+      try {
+        await reauthenticateWithCredential(user, credentials);
+        if (phone) {
+          await updateProfilePhone(phone, loggedInUser.username);
+          alert("Congratulations! You successfully changed your phone!");
+          const allUsers = await getAllUsers();
+          setUser((prev) => ({ ...prev, allUsers }));
+          setPhone(phone);
+        }
+      } catch (error) {
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
     }
-  }
-};
+  };
+
+  return (
+    <>
+    <Form.Label htmlFor="" style={{ fontSize: "20px", color: "gray", fontWeight: "normal", paddingTop: "40px", }}>
+            Want to change your phone number?
+          </Form.Label>
+          <Card>
+            <Card.Body>
+              <Form.Group controlId="confirmPhone">
+                <Form.Label style={{ marginTop: "8px" }}>Change phone number</Form.Label>
+                <Form.Control
+                  type="phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                  placeholder="New phone"
+                />
+                <Button onClick={changePhone} style={{ margin: "0 auto", display: "block", marginTop: "15px", fontSize: "17px", }}>
+                  Update Phone
+                </Button>
+              </Form.Group>
+            </Card.Body>
+          </Card>
+    </>
+  )
+}
