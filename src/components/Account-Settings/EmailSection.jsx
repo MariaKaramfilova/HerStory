9e7ac720d6b76e7dcbfc9829836ee518a7873ba5
Hristiyan Fachikov <hristiyan.fachikov>
2023-08-15 21:16:07 +0300

    Adding an EmailSection component

--- src/components/Account-Settings/EmailSection.jsx
+++ src/components/Account-Settings/EmailSection.jsx
@@ -1,24 +1,79 @@
 import { updateEmail, reauthenticateWithCredential } from "firebase/auth";
 import { EmailAuthProvider } from "firebase/auth";
 import { updateProfileEmail } from "../../services/users.services";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
+import { getAllUsers } from "../../services/users.services";
+import { Button, Card, Form, Alert } from 'react-bootstrap';
 
-export const EmailChanging = async (user, loggedInUser, email, username) => {
-  const password = prompt("Please enter your password to confirm email change:");
 
-  if (password) {
-    const credentials = EmailAuthProvider.credential(loggedInUser.email, password);
-    try {
-      await reauthenticateWithCredential(user, credentials);
-      if (email) {
-        await updateEmail(user, email);
-        await updateProfileEmail(email, username);
-        return null;
+export default function EmailSection({ loggedInUser, user, setUser }) {
+
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  const changeEmail = async () => {
+    const password = prompt(
+      "Please enter your password to confirm email change:"
+    );
+    if (password) {
+      const credentials = EmailAuthProvider.credential(loggedInUser.email, password);
+      try {
+        await reauthenticateWithCredential(user, credentials);
+        if (email) {
+          await updateEmail(user, email);
+          alert("Congratulations! You successfully changed your email!");
+          const allUsers = await getAllUsers();
+          setUser((prev) => ({ ...prev, allUsers }));
+          await updateProfileEmail(email, loggedInUser.username);
+          setEmail("");
+          setEmailError("");
+        }
+      } catch (error) {
+        setEmailError("Invalid email or password! Please try again.");
+        console.error(error);
       }
-    } catch (error) {
-      console.error(error);
-      return "Invalid email or password! Please try again.";
     }
-  }
+  };
 
-  return "An error occurred while changing the email. Please try again.";
-};
+  return (
+    <div>
+      <Form.Label
+        htmlFor=""
+        style={{
+          fontSize: "20px",
+          color: "gray",
+          fontWeight: "normal",
+          paddingTop: "40px",
+        }}
+      >
+        Want to change your email?
+      </Form.Label>
+      <Card>
+        <Card.Body>
+          <Form.Group controlId="confirmEmail">
+            <Form.Label style={{ marginTop: "8px" }}>Change email</Form.Label>
+            <Form.Control
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="New email"
+            />
+            {emailError && <Alert>{emailError}</Alert>}
+            <Button
+              onClick={changeEmail}
+              style={{
+                margin: "0 auto",
+                display: "block",
+                marginTop: "15px",
+                fontSize: "17px",
+              }}
+            >
+              Update Email
+            </Button>
+          </Form.Group>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
